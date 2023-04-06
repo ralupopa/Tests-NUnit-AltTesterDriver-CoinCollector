9ce223b1524de45728c1b4a77d4c12661886e14c
ALTOM-PC63\raluca.popa <raluca.popa@altom.com>
2023-04-06 19:12:30 +0300

    test game displayed, reorganize altDriver used in Helper

--- CoinCollector.Tests/GamePlayTests.cs
+++ CoinCollector.Tests/GamePlayTests.cs
@@ -8,6 +8,7 @@ public class GamePlayTests: BaseTest
   {
     altDriver = new AltDriver();
     helperMethods = new HelperMethods(altDriver);
+    helperMethods.LoadScene();
   }
 
   [OneTimeTearDown]
@@ -17,42 +18,27 @@ public class GamePlayTests: BaseTest
   }
 
   [Test]
-  public void TestStartGame()
+  public void TestGamePlayDisplayedCorrectly()
   {
-    altDriver.LoadScene("GameScene");
+    Assert.True(helperMethods.IsDisplayed());
+  }
 
-    Thread.Sleep(1000);
-    altDriver.Scroll(2000, 0.1f, true);
-
-    Thread.Sleep(500);
-    helperMethods.AssertCoinIsCollected();
-
+  [Test]
+  public void TestTimeLeftToPlayIsDisplayed()
+  {
     var timeValue = helperMethods.GetCurrentTime();
     Console.WriteLine("Value of time in seconds: " + timeValue);
   }
 
   [Test]
-  public void TestGeneratedWithRecorder()
+  public void TestToIncreaseTimeLeftToPlay()
   {
-    altDriver.LoadScene("GameScene");
-
+    
     Thread.Sleep(1000);
     altDriver.PressKey(AltKeyCode.Mouse0);
     Thread.Sleep(500);
     helperMethods.AssertCoinIsCollected();
-    // altDriver.KeyDown(AltKeyCode.Mouse0,1);
-    // altDriver.MoveMouse(new AltVector2(968.0593f,227.4916f),0.01f);
-    // altDriver.KeyUp(AltKeyCode.Mouse0);
-    // altDriver.KeyDown(AltKeyCode.Mouse0,1);
-    // altDriver.KeyUp(AltKeyCode.Mouse0);
-    // altDriver.MoveMouse(new AltVector2(1102.335f,235.5524f),0.01f);
-    // altDriver.KeyUp(AltKeyCode.RightArrow);
-    // altDriver.KeyDown(AltKeyCode.UpArrow,1);
-    // altDriver.KeyUp(AltKeyCode.UpArrow);
-    // altDriver.KeyDown(AltKeyCode.Mouse0,1);
-    // altDriver.KeyUp(AltKeyCode.Mouse0);
-    // altDriver.KeyDown(AltKeyCode.RightArrow,1);
-    // altDriver.KeyDown(AltKeyCode.Mouse0,1);
-    // altDriver.KeyUp(AltKeyCode.Mouse0);
+
+    helperMethods.SetCurrentTime();
   }
 }
--- CoinCollector.Tests/HelperMethods.cs
+++ CoinCollector.Tests/HelperMethods.cs
@@ -4,8 +4,25 @@ public class HelperMethods: BaseTest
     public HelperMethods(AltDriver driver)
     {
     }
-    public AltObject TimeValueText { get => BaseTest.altDriver.WaitForObject(By.NAME, "TimeValueText"); }
-    public AltObject CoinValueText { get => BaseTest.altDriver.WaitForObject(By.NAME,"CoinValueText"); }
+    public void LoadScene()
+    {
+        altDriver.LoadScene("GameScene");
+    }
+    public AltObject GameView { get => altDriver.WaitForObject(By.NAME, "GameView", timeout: 10); }
+    public AltObject TimeValueText { get => altDriver.WaitForObject(By.NAME, "TimeValueText"); }
+    public AltObject CoinValueText { get => altDriver.WaitForObject(By.NAME,"CoinValueText"); }
+    public AltObject Player { get => altDriver.WaitForObject(By.NAME, "Player"); }
+    public AltObject Hill { get => altDriver.WaitForObject(By.NAME, "Hill"); }
+    public AltObject CoinSpawner { get => altDriver.WaitForObject(By.NAME, "CoinSpawner"); }
+    public AltObject SpawnPoint { get => altDriver.WaitForObject(By.NAME, "SpawnPoint"); }
+
+    public bool IsDisplayed()
+    {
+        if (GameView != null && TimeValueText != null && CoinValueText != null
+        && Player != null && Hill != null && CoinSpawner != null &&  SpawnPoint != null)
+            return true;
+        return false;
+    }
     public void AssertCoinIsCollected()
     {
         Assert.AreEqual("1", CoinValueText.GetText());
@@ -14,5 +31,18 @@ public class HelperMethods: BaseTest
     public int GetCurrentTime() 
     {
         return TimeValueText.GetComponentProperty<int>("UnityEngine.UI.Text", "text", "UnityEngine.UI");
+    }
+
+    public void SetCurrentTime()
+    {
+        if (TimeValueText != null)
+        {
+            TimeValueText.SetComponentProperty("UnityEngine.UI.Text", "text", 100, "UnityEngine.UI");
+            Console.WriteLine(GetCurrentTime());
+        }
+        else
+        {
+            Console.WriteLine("Could not set Time Value Component");
+        }
     }
 }
